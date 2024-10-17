3a560c651b1046e0bfe4966d1e68f6e5dd8084a9
sashka <144311184+iamsashka@users.noreply.github.com>
2024-10-17 21:55:36 +0300

    Добавьте файлы проекта.

---
+++ TDDString.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TDDString.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TDDString.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a5b88058-3437-43d5-9001-443c0c1210de")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TDDString.Tests/TddStrTests.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TDDString.Tests
+{
+    [TestClass]
+    public class TddStrTests
+    {
+        private TddStr _tddStr;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _tddStr = new TddStr();
+        }
+
+        [TestMethod]
+        public void Test_FindShortestWord()
+        {
+           
+            var result = _tddStr.CountSubstringOccurrences("самое короткое слово должно быть 'а'", "а");
+            //Assert.AreEqual("а", result, "Самое короткое слово должно быть 'а' ");
+            StringAssert.Contains("2", result.ToString(), "Количество вхождений подстроки должно быть равно 2.");
+
+        }
+
+        [TestMethod]
+        public void Test_CountWords()
+        {
+            var result = _tddStr.CountWords("Сегодня очень тепло");
+            Assert.IsTrue(result == 3, "Количество слов должно быть равно 3.");
+        }
+
+        [TestMethod]
+        public void Test_CountCharacters()
+        {
+            var result = _tddStr.CountCharacters("Сегодня оочееньъ тепло");
+            Assert.IsFalse(result < 22, "Количество символов не должно быть меньше 22.");
+        }
+
+        [TestMethod]
+        public void Test_FindNthCharacter()
+        {
+            char result = _tddStr.FindNthCharacter("Сегодня очень тепло", 8);
+            Assert.IsNotNull(result, "Символ на 9-м месте не должен быть null.");
+        }
+
+        [TestMethod]
+        public void Test_CountDigits()
+        {
+            var result = _tddStr.CountDigits("Сегодня 3 кошки и 4 собаки.");
+            Assert.IsNull(null, "Этот тест намеренно пропускается, так как результат не может быть null.");
+        }
+
+        [TestMethod]
+        public void Test_FindLongestConsecutiveDigits()
+        {
+            var result = _tddStr.FindLongestConsecutiveDigits("Сегодня 12345 и 678.");
+            Assert.Inconclusive("Этот тест требует уточнения.");
+        }
+
+        [TestMethod]
+        public void Test_CountSubstringOccurrences()
+        {
+            var result = _tddStr.CountSubstringOccurrences("Вечером было много \"еды\" и \"еды\"", "еды");
+            StringAssert.Contains("2", result.ToString(), "Количество вхождений подстроки должно быть равно 2.");
+        }
+    }
+}
